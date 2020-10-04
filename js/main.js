3030c7d5103b0b425a6d91f7253908385b5fa741
Andrey Ovcharov (Datosh Code) <datoshcode@gmail.com>
2020-10-04 09:45:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  $('.slider__inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+  });
+
+  $('select').styler();
+
+  $('.header__btn-menu').on('click', function(){
+    $('.top-menu ul').slideToggle();
+    $('.menu-panel').slideToggle();
+  });
+
+});
